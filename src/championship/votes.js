073f207ef21ac7a79713e40e33c89908bfdfa105
HarshKhandeparkar <mjkgoa@gmail.com>
2020-06-16 01:26:45 +0530

    fix: display voterlist actually

--- src/championship/votes.js
+++ src/championship/votes.js
@@ -1,6 +1,7 @@
 const { prefix } = require('../config.json');
 const { MessageEmbed } = require('discord.js');
 const getStats = require('./getChampionshipStats');
+const getVoterList = require('./getVoterList');
 
 function setVotes(client, championshipMeta) {
   const { colosseumChannel, upvoteEmoji, downvoteEmoji, championshipName } = championshipMeta;
@@ -8,7 +9,7 @@ function setVotes(client, championshipMeta) {
   client.on('message', msg => {
     if (msg.content.trim().toLowerCase() == `${prefix}champ ${championshipName}`) {
       // Championship Current Stats
-      msg.channel.send('Generating competition stats. This can (and will) take time.');
+      msg.channel.send('Generating championship stats. This can (and will) take time.');
       getStats(client, championshipMeta, finalMemes => {
         finalMemes.forEach(meme => {
           msg.channel.send(
@@ -30,7 +31,14 @@ function setVotes(client, championshipMeta) {
       msg.react(upvoteEmoji);
       msg.react(downvoteEmoji);
     }
-    else if (msg.content.trim().toLowerCase() == `${prefix}voterlist ${championshipName}`);
+    else if (msg.content.trim().toLowerCase() == `${prefix}voterlist ${championshipName}`) {
+      getVoterList(client, championshipMeta, voterIds => {
+        let voterList = `\n**${championshipName.toUpperCase()}** VOTERS:\n`;
+
+        voterIds.forEach(id => voterList += `- <@${id}> \n`);
+        msg.channel.send(voterList);
+      })
+    }
   })
 }
 
