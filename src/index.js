9765c49058fbfb30aba6a499c2a0d69f673d2a71
Harsh <mjkgoa@gmail.com>
2020-06-18 10:59:26 +0530

    feat: use the local config file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 const discord = require('discord.js');
 require('dotenv').config();
+const setParticipation = require('./championship/participate');
+const setVotes = require('./championship/votes');
+const updateList = require('./championship/updateList');
 
-const setPhase1 = require('./lbryeme-championship/setPhase1');
-//const setChampionship = require('./example-championship/setExampleChampionship');
-
+const championshipsList = require(process.env.championships_list_loc);
 const client = new discord.Client();
 
-setPhase1(client);
-//setChampionship(client);
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}`);
+	
+	for (let championship in championshipsList) {
+		console.log(`Setting up ${championship}`, championshipsList[championship]);
+		
+		setVotes(client, championshipsList[championship]);
+		if (championshipsList[championship].allowParticipationCommands) setParticipation(client, championshipsList[championship]);
+		updateList(client, championshipsList[championship]);
+	}
+})
 
 const tryLogin = () => {
 	console.log('login failed. Trying again.');
 
-	setTimeout(() => client.login(process.env.token).catch(tryLogin), 1000)
+	setTimeout(() => client.login(process.env.token).catch(tryLogin), 1000);
 }
 
 client.login(process.env.token).catch(tryLogin);
